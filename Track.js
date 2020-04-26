962883155ce9f06a9046d6800dd088ac78c04a5d
bmv3 <bmv3@mail.ru>
2020-04-27 01:02:46 +0500

    Remove trash

--- Track.js
+++ Track.js
@@ -14,35 +14,7 @@ class Track {
     this.gainNode.connect(this.context.destination);
     this.gainNode.gain.setValueAtTime(1, this.context.currentTime);
 
- }
-
-  // loadTrack(url) {
-  //   let source = this.context.createBufferSource();
-
-  //   let thisBuffer = this;
-
-  //   return fetch(url)
-  //     .then(function(response) {
-  //       if (!response.ok) {
-  //         throw new Error("HTTP error, status = " + response.status);
-  //       }
-  //       return response.arrayBuffer();
-  //     })
-  //     .then(function(buffer) {
-  //       thisBuffer.context.decodeAudioData(buffer, function(decodedData) {
-  //         thisBuffer.source.buffer = decodedData;
-  //         thisBuffer.source.connect(thisBuffer.context.destination);
-  //       });
-  //     });
-  // }
-
-  // setup() {
-  //   this.gainNode = this.context.createGain();
-  //   this.source = this.context.createBufferSource();
-  //   this.source.buffer = this.buffer;
-  //   this.source.connect(this.gainNode);
-  //   this.gainNode.connect(this.context.destination);
-  // }
+  }
 
   play() {
     this.myAudio.play();
@@ -51,7 +23,7 @@ class Track {
   stop() {
     this.myAudio.pause();
     this.myAudio.src = '';
-}
+  }
   
   pause() {
     this.myAudio.pause();
@@ -60,7 +32,6 @@ class Track {
   soft(long) {
     this.gainNode.gain.linearRampToValueAtTime(0, this.context.currentTime + long);
   }
-
 
   setVolume(volume) {
     this.myAudio.volume = volume / 100;
